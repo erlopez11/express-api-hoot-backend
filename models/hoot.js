6a20c4796d45913c6d60702c75cf6aeda084c7a5
erlopez11 <lopezelainy@gmail.com>
2025-03-31 12:01:51 -0500

    wip: defines hoot model

---
+++ models/hoot.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const hootSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    text: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+        enum: ['News', 'Sports', 'Games', 'Movies', 'Music', 'Television'],
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User', //ref propperty allows us to use .populate
+    },
+    
+}, {timestamps: true});
+
+const Hoot = mongoose.model('Hoot', hootSchema);
+module.exports = Hoot;
